dad6e60a558f5a3ab8a77ec62e820cf69ac3f4ad
icoon22 <icoon22@gmail.com>
2018-03-28 14:22:26 +0900

    최초저장

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        vendor: ['jquery', './src/assets/vendor/sample-vendor.js'],
+        main: ['./src/main.js']
+    },
+    output: {
+        filename: '[name]-bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: "/"
+    },
+    devServer: {
+        contentBase: 'dist',
+        overlay: true,
+        stats: {
+            colors: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [               
+                    {
+                        loader: 'style-loader'
+                    },
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].html"
+                        }
+                    },
+                    {
+                        loader: "extract-loader"
+                    },
+                    {
+                        loader: "html-loader",
+                        options: {
+                            attrs: ["img:src"],
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|gif|png)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "assets/images/[name]-[hash:8].[ext]"
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name]-bundle.css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
